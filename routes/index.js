3548abe51fc62173ec3fc32bb52a8b6f6e4d9421
Daoud Merchant <merchantdaoud@hotmail.com>
2022-01-27 19:02:10 +0100

    Render input errors

--- routes/index.js
+++ routes/index.js
@@ -10,22 +10,29 @@ router.get("/", function (req, res, next) {
 
 router.post("/", [
   // validate and sanitise text inputs
-  body("content", "Invalid main video link")
+  body("content")
     .trim()
     .isURL()
+    .withMessage("Main video mmust be a valid URL")
     .matches(/m3u8$/)
+    .withMessage("Main video must be a valid playlist file")
     .escape(),
-  body("ad", "Invalid advert video link")
+  body("ad")
     .trim()
     .isURL()
+    .withMessage("Advert must be a valid URL")
     .matches(/,3u8$/)
+    .withMessage("Advert must be a valid playlist file")
     .escape(),
+
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      // Has errors, rerender form
       res.render("index", { ...req.body, errors: errors.array() });
       return;
     }
+    // No errors, proceed with splicing
   },
 ]);
 
