04646bd296e20f7147aa096870ffda9a3a7f49cb
Daoud Merchant <merchantdaoud@hotmail.com>
2022-01-27 18:49:52 +0100

    Escape and sanitise form data, catch form errors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
+const { body, validationResult } = require("express-validator");
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.post("/", [
+  // validate and sanitise text inputs
+  body("content", "Invalid main video link")
+    .trim()
+    .isURL()
+    .matches(/m3u8$/)
+    .escape(),
+  body("ad", "Invalid advert video link")
+    .trim()
+    .isURL()
+    .matches(/,3u8$/)
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("index", { ...req.body, errors: errors.array() });
+      return;
+    }
+  },
+]);
 
 module.exports = router;
